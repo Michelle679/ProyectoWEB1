4718a8028374fafc34882f5ffce130da102bcd09
Michelle679 <mhernandezv@ucenfotec.ac.cr>
2025-10-13 16:43:38 -0600

    Primer commit

---
+++ js/login.js
@@ -0,0 +1,11 @@
+ <script>
+    const usuario = document.getElementById('usuario');
+    const clave = document.getElementById('clave');
+
+    if (usuario === 'cenfo' && clave === '123') {
+        mensaje = 'Acceso concedido'};{href = 'landing.html'
+        }
+    else {
+        mensaje = 'Usuario o contraseña incorrectos'};
+  </script>
+
